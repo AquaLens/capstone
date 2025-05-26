6e1cbda75f4a140f27d69aebb801cf1b750f62c0
Celia <celia.meissner@student.uva.nl>
2025-05-26 19:13:37 +0200

    js edits dashboard

--- docs/dashboard/citites.js
+++ docs/dashboard/citites.js
@@ -173,3 +173,11 @@ let cityCache = {
 };
 
 console.log(`Predefined cities loaded: ${Object.keys(cityCache).length} cities available`);
+
+// Debug: test if it works
+console.log('Sample predefined cities:', {
+  'danube river': cityCache['danube river'],
+  'montana': cityCache['montana'], 
+  'medmerry': cityCache['medmerry'],
+  'story mill': cityCache['story mill']
+});
